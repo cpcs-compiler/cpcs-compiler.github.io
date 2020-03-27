ead2a967d14a044981d2cab32ebbca0f86fd8298
d0p1 <contact@d0p1.eu>
2020-03-27 10:15:57 +0100

    use browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,21 @@ const uglify = require("gulp-uglify-es").default;
 const sass = require("gulp-sass");
 const concat = require("gulp-concat");
 const cssnano = require("gulp-cssnano");
+const browsersync = require("browser-sync");
 
 const buildCss = () =>
-  src("src/scss/style.scss").pipe(sass()).pipe(cssnano()).pipe(dest("dist/assets"));
+  src("src/scss/style.scss")
+    .pipe(sass())
+    .pipe(cssnano())
+    .pipe(dest("dist/assets"))
+    .pipe(browsersync.reload({ stream: true }));
 
 const buildJs = () =>
   src("src/scripts/*.js")
     .pipe(concat("script.js"))
     .pipe(uglify())
-    .pipe(dest("./dist/assets"));
+    .pipe(dest("./dist/assets"))
+    .pipe(browsersync.reload({ stream: true }));
 
 const buildHtml = () =>
   src("src/pages/*.njk")
@@ -21,17 +27,25 @@ const buildHtml = () =>
         path: ["src/templates"],
       })
     )
-    .pipe(dest("./dist"));
+    .pipe(dest("./dist"))
+    .pipe(browsersync.reload({ stream: true }));
 
-const watchRules = () => {
-  watch(["src/pages/*.njk"], buildHtml);
-  watch(["src/scripts/*.ls"], buildJs);
-  watch(["src/scss/*.scss", "src/scss/**/*.scss"], buildCss);
-};
-
-task("watch", watchRules);
+watch(["src/pages/*.njk"], buildHtml);
+watch(["src/scripts/*.ls"], buildJs);
+watch(["src/scss/*.scss", "src/scss/**/*.scss"], buildCss);
+watch(["dist/*"]).on("change", browsersync.reload);
 
 task("html", buildHtml);
 task("js", buildJs);
 task("css", buildCss);
 task("build", parallel(buildHtml, buildCss, buildJs));
+task("serve", () =>
+  browsersync.init({
+    server: {
+      baseDir: "./dist",
+      index: "index.html",
+    },
+    notify: false,
+    injectChanges: true,
+  })
+);
